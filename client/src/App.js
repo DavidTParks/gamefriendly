8f403a91600a4f26d50852df303c13ca99335d6b
DavidTParks <davidthomasparks@gmail.com>
2017-01-26 16:45:53 -0800

    beginning signup page

--- client/src/App.js
+++ client/src/App.js
@@ -13,9 +13,61 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div classname="Signup-Box">
+          <SignUpContainer />
+        </div>
       </div>
     );
   }
 }
+class SignUpContainer extends React.Component {
+	render() {
+		return (
+			<div id='signUpContainer'>
+				<SignUpHeader title="Sign Up" />
+				<SignUpForm />
+			</div>
+		)
+	}
+}
+
+const SignUpHeader = props => (
+	<div id='signUpHeader'>
+		<div id='signUpHeaderTitle'>
+			{props.title}
+		</div>
+	</div>
+);
+
+const FormInput = props => (
+	<div className='signUpRow'>
+		<input type={props.type} placeholder={props.placeholder} />
+	</div>
+);
+
+const FormCheckBox = props => (
+	<div className='signUpRow'>
+		<input id={props.id} type='checkbox' />
+		<label htmlFor={props.id}>{props.label}</label>
+	</div>
+);
+
+const FormButton = props => (
+	<div className='signUpRow'>
+		<button type='button'>{props.title}</button>
+	</div>
+);
+
+const SignUpForm = props => (
+	<div id='signUpFormContainer'>
+		<form id="signUpForm">
+			<FormInput type="text" placeholder="email" />
+				<FormInput type="password" placeholder="password" />
+				<FormInput type="password" placeholder="confirm" />
+				<FormCheckBox id="terms" label="I agree to the terms and conditions" />
+				<FormButton title="Sign Up" />
+		</form>
+	</div>
+);
 
 export default App;
