a26bb966244d0166d535c67a2606cb7ed8785fe4
DavidTParks <davidthomasparks@gmail.com>
2017-02-02 18:56:52 -0800

    code cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import * as ReactBootstrap from "react-bootstrap";
 import GameSessionCard from './GameSessionCard';
 import Searchbar from './Searchbar';
 import Navigation from './Navigation';
 import Jumbo from './Jumbo';
-
-var Navbar = ReactBootstrap.Navbar,
-Nav = ReactBootstrap.Nav,
-NavDropdown = ReactBootstrap.NavDropdown,
-NavItem = ReactBootstrap.NavItem,
-DropdownButton = ReactBootstrap.DropdownButton,
-FormGroup = ReactBootstrap.FormGroup,
-Jumbotron = ReactBootstrap.Jumbotron,
-PageHeader = ReactBootstrap.PageHeader,
-Button = ReactBootstrap.Button,
-FormControl = ReactBootstrap.FormControl,
-Row = ReactBootstrap.Row,
-Thumbnail = ReactBootstrap.Thumbnail,
-Col = ReactBootstrap.Col,
-FormControl = ReactBootstrap.FormControl,
-MenuItem = ReactBootstrap.MenuItem;
 
 class App extends Component {
   render() {
