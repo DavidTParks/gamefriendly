6f22bfab0f00084b94c4c8967027bca480f04184
DavidTParks <davidthomasparks@gmail.com>
2017-03-02 13:09:05 -0800

    trying out logout sidebar

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { Sidebar, SidebarItem } from 'react-responsive-sidebar';
+
+const items = [
+  <SidebarItem>Dashboard</SidebarItem>,
+  <SidebarItem>Profile</SidebarItem>,
+  <SidebarItem>Settings</SidebarItem>,
+  <SidebarItem>Logout</SidebarItem>,
+];
 
 ReactDOM.render(
-  <App />,
+  <Sidebar background="#8064A2" closeOnBackdropClick={true} width="200" content={items}>
+    <App />
+  </Sidebar>,
   document.getElementById('root')
 );
